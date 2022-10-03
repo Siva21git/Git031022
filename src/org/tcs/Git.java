24bb922a3d745c26c1cda5e650e67b75862775c9
Siva21git <siva03371@gmail.com>
2022-10-02 19:26:00 -1200

    Git

---
+++ src/org/tcs/Git.java
@@ -0,0 +1,13 @@
+package org.tcs;
+
+public class Git {
+	private void employee() {
+		System.out.println("Employee Name = Siva");
+
+	}
+	public static void main(String[] args) {
+		Git g = new Git();
+		g.employee();
+	}
+
+}
